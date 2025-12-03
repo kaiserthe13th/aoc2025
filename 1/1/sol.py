32121bfee8ffaa7a51114e98a407ab37322ebda5
Kerem Göksu <superkerem13@gmail.com>
2025-12-03 22:45:53 +0300

    add comments to day 1 to make the logic clearer

--- 1/1/sol.py
+++ 1/1/sol.py
@@ -1,15 +1,19 @@
 import os
 
+# load input file
 with open(os.path.join(__file__, "../../input")) as f:
     inp = [line.strip() for line in f.readlines()]
 
 res = 0
 
-rot = 50
+rot = 50 # starting rotation
 for line in inp:
+    # get value
     val = int(line[1:])
     if line.startswith('L'):
+        # make positive
         val = 100 - val
+    # apply mod rotation to get it to wrap-around
     rot = (rot + val) % 100
     if not rot:
         res += 1
--- 1/2/sol.py
+++ 1/2/sol.py
@@ -1,13 +1,14 @@
 import os
 
+# load input file
 with open(os.path.join(__file__, "../../input")) as f:
     inp = [line.strip() for line in f.readlines()]
 
 res = 0
-rot = 50
-size = 100
+rot = 50 # starting rotation
+size = 100 # it was getting out of hand, so made it a variable
 for line in inp:
-    dir = line[0]
+    dir = line[0] # separate direction
     val = int(line[1:])
     res += val // size
     val %= size
@@ -15,9 +16,12 @@ for line in inp:
     if dir == 'L':
         val *= -1
     
+    # this actually took a while to come up
+    # if we passed the dial (+), or we had positive rotation and we passed the dial in reverse
     if rot + val >= size or (rot > 0 and rot + val <= 0):
         res += 1
 
+    # apply mod rotation to get it to wrap-around
     rot = (rot + val) % size
 
 print(res)
