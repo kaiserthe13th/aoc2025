940fe8e4bd1be032a474a8841db26ad8434774b1
Kerem Göksu <superkerem13@gmail.com>
2025-12-03 23:43:42 +0300

    add comments to day 2 to make the logic clearer

--- 2/1/sol.py
+++ 2/1/sol.py
@@ -1,22 +1,25 @@
 import os
 
+# load input file
 with open(os.path.join(__file__, "../../input")) as f:
     inp = [line.strip().split('-') for line in f.read().split(',')]
 
 res = 0
-used = set()
+used = set() # to prevent reuse
 for [a, b] in inp:
     start = int(a)
     stop = int(b)
     r = range(start, stop + 1)
-    exp = 10 ** (len(a) // 2)
-    for i in range(start, stop + exp, exp):
+    exp = 10 ** (len(a) // 2) # find the increment amount
+    for i in range(start, stop + exp, exp): # iterate over the numbers that we can test candidates in
         s = str(i)
         ldiv, lmod = divmod(len(s), 2)
         if lmod == 1:
+            # if it ain't zero, that means there is no way to split the number into
+            # fixed-size non-overlapping chunks
             continue
-        ck = int(s[:ldiv] * 2)
-        if ck in r and ck not in used:
+        ck = int(s[:ldiv] * 2) # find what we get if we repeated the initials
+        if ck in r and ck not in used: # if in range and not used already
             used.add(ck)
             res += ck
 
--- 2/2/sol.py
+++ 2/2/sol.py
@@ -1,27 +1,37 @@
 import os
 
+# load input file
 with open(os.path.join(__file__, "../../input")) as f:
     inp = [line.strip().split('-') for line in f.read().split(',')]
 
 res = 0
-used = set()
+used = set() # to prevent reuse
 for [a, b] in inp:
     start = int(a)
     stop = int(b)
     r = range(start, stop + 1)
     def try_split(split_count: int):
+        # so the logic here is the split_count is the following:
+        # - we find an increment which is the step we use
+        #   and it is selected to be whatever the following diagram:
+        # 
+        #   for example if we had '481293':
+        #       with split_count = 2 => gives us 10 ** (6 - 6 // 2) = 10 ** 3 = 1000  as it splits it into '481' '293'
+        #       with split_count = 3 => gives us 10 ** (6 - 6 // 3) = 10 ** 4 = 10000 as it splits it into '48' '12' '93'
         global res
-        exp = 10 ** (len(a) // split_count)
-        for i in range(start, stop + exp, exp):
+        exp = 10 ** (len(a) - len(a) // split_count) # find the increment amount
+        for i in range(start, stop + exp, exp): # iterate over the numbers that we can test candidates in
             s = str(i)
             ldiv, lmod = divmod(len(s), split_count)
             if lmod != 0:
+                # if it ain't zero, that means there is no way to split the number into
+                # fixed-size non-overlapping chunks
                 continue
-            ck = int(s[:ldiv] * split_count)
-            if ck in r and ck not in used:
+            ck = int(s[:ldiv] * split_count) # find what we get if we repeated the initials
+            if ck in r and ck not in used: # if in range and not used already
                 used.add(ck)
                 res += ck
-    for i in range(2, len(b) + 1):
+    for i in range(2, len(b) + 1): # try to split the number into 2, 3, 4, ..., up to and including len(b) parts and try
         try_split(i)
 
 print(res)
